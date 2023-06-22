9e4b73a66a26d94e3ff1a0732606f37f8d63e6ca
dinosaturnus <alinakrivosheeva99@gmail.com>
2023-06-22 22:51:14 +0300

    added allure lambda steps

--- src/test/java/demoqa/tests/RegistrationPracticeFormTest.java
+++ src/test/java/demoqa/tests/RegistrationPracticeFormTest.java
@@ -1,10 +1,13 @@
 package demoqa.tests;
 
 import demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static demoqa.tests.TestData.*;
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationPracticeFormTest extends TestBase {
 
@@ -12,35 +15,39 @@ public class RegistrationPracticeFormTest extends TestBase {
 
     @Test
     @Tag("remote")
+    @DisplayName("Successful registration form test")
     void fullFormTest() {
-
-
-        registrationPage.openPage()
-                .closedBanners()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(email)
-                .setGender(gender)
-                .setPhoneNumber(phoneNumber)
-                .setBirthDate(birthYear, birthMonth, birthDay)
-                .setSubject(subject)
-                .setHobby(hobby)
-                .uploadPicture(picturePath)
-                .setAddress(address)
-                .setState(state)
-                .setCity(city)
-                .clickSubmitButton();
-
-        registrationPage.verifyRegistrationsModalAppears()
-                .verifyResults("Student Name", firstName + " " + lastName)
-                .verifyResults("Student Email", email)
-                .verifyResults("Gender", gender)
-                .verifyResults("Mobile", phoneNumber)
-                .verifyResults("Date of Birth", birthDay + " " + birthMonth + "," + birthYear)
-                .verifyResults("Subjects", subject)
-                .verifyResults("Hobbies", hobby)
-                .verifyResults("Picture", pictureName)
-                .verifyResults("Address", address)
-                .verifyResults("State and City", state + " " + city);
+        step("Open form", () -> {
+            registrationPage.openPage();
+        });
+        step("Fill form", () -> {
+            registrationPage.closedBanners()
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setUserEmail(email)
+                    .setGender(gender)
+                    .setPhoneNumber(phoneNumber)
+                    .setBirthDate(birthYear, birthMonth, birthDay)
+                    .setSubject(subject)
+                    .setHobby(hobby)
+                    .uploadPicture(picturePath)
+                    .setAddress(address)
+                    .setState(state)
+                    .setCity(city)
+                    .clickSubmitButton();
+        });
+        step("Verify results", () -> {
+            registrationPage.verifyRegistrationsModalAppears()
+                    .verifyResults("Student Name", firstName + " " + lastName)
+                    .verifyResults("Student Email", email)
+                    .verifyResults("Gender", gender)
+                    .verifyResults("Mobile", phoneNumber)
+                    .verifyResults("Date of Birth", birthDay + " " + birthMonth + "," + birthYear)
+                    .verifyResults("Subjects", subject)
+                    .verifyResults("Hobbies", hobby)
+                    .verifyResults("Picture", pictureName)
+                    .verifyResults("Address", address)
+                    .verifyResults("State and City", state + " " + city);
+        });
     }
 }
