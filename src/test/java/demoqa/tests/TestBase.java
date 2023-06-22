30ca69d1c360e2ee1f1c6ece57569e212314beae
dinosaturnus <alinakrivosheeva99@gmail.com>
2023-06-22 22:41:38 +0300

    added listener

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -1,7 +1,11 @@
 package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -23,4 +27,11 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
     }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+
 }
