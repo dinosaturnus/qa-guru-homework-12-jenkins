33b794bd0ce236cab8cdeccd41d11447cd426a3d
dinosaturnus <alinakrivosheeva99@gmail.com>
2023-06-27 01:12:29 +0300

    added properties

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import properties.tests.SystemProperties;
+import properties.SystemProperties;
 
 import java.util.Map;
 
--- src/test/java/properties/SystemProperties.java
+++ src/test/java/properties/SystemProperties.java
@@ -1,4 +1,4 @@
-package properties.tests;
+package properties;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
