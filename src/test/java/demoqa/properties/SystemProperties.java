2a29bf8fd460b00ef4d6b3b459f88c16ec334f58
dinosaturnus <alinakrivosheeva99@gmail.com>
2023-06-27 01:24:47 +0300

    added properties

--- src/test/java/demoqa/properties/SystemProperties.java
+++ src/test/java/demoqa/properties/SystemProperties.java
@@ -1,15 +1,17 @@
 package demoqa.properties;
 
+import org.junit.jupiter.api.DisplayName;
+
 import static java.lang.String.format;
 
 
 public class SystemProperties {
 
-    public static String browserProperty = System.getProperty("browser", "chrome");
-    public static String browserSizeProperty = System.getProperty("browserSize", "1920x1080");
-    public static String browserVersionProperty = System.getProperty("browserVersion", "112.0.5615.137");
-    public static String baseUrlProperty = System.getProperty("baseUrl", "https://demoqa.com");
-    public static String remoteSelenoidProperty = System.getProperty("remoteSelenoidUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+    public static String browserProperty = System.getProperty("browser", "chrome"),
+            browserSizeProperty = System.getProperty("browserSize", "1920x1080"),
+            browserVersionProperty = System.getProperty("browserVersion", "112.0.5615.137"),
+            baseUrlProperty = System.getProperty("baseUrl", "https://demoqa.com"),
+            remoteSelenoidProperty = System.getProperty("remoteSelenoidUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
 
 /*
