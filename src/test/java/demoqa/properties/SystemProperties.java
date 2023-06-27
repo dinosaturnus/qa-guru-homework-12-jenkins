80788165e4f7b5cc720c32a042b168d7270134d4
dinosaturnus <alinakrivosheeva99@gmail.com>
2023-06-27 10:18:55 +0300

    added properties

--- src/test/java/demoqa/properties/SystemProperties.java
+++ src/test/java/demoqa/properties/SystemProperties.java
@@ -1,10 +1,5 @@
 package demoqa.properties;
 
-import org.junit.jupiter.api.DisplayName;
-
-import static java.lang.String.format;
-
-
 public class SystemProperties {
 
     public static String browserProperty = System.getProperty("browser", "chrome"),
@@ -12,55 +7,5 @@ public class SystemProperties {
             browserVersionProperty = System.getProperty("browserVersion", "112.0.5615.137"),
             baseUrlProperty = System.getProperty("baseUrl", "https://demoqa.com"),
             remoteSelenoidProperty = System.getProperty("remoteSelenoidUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-
-
-/*
-    @Test
-    void systemPropertiesTest1() {
-        System.setProperty("browser", "chrome"); // внутренне хранилище ключей-значений в джаве
-        String browser = System.getProperty("browser");
-
-        System.out.println(browser); // chrome
-    }
-
-    @Test
-    void systemPropertiesTest2() {
-        String browser = System.getProperty("browser", "mozilla");
-
-        System.out.println(browser); // chrome
-    }
-
-    @Test
-    void systemPropertiesTest3() {
-        System.setProperty("browser", "chrome");
-        String browser = System.getProperty("browser", "mozilla");
-
-        System.out.println(browser); // chrome
-    }
-
-    @Test
-    @Tag("test")
-    void systemPropertiesTest4() {
-        String browser = System.getProperty("browser", "mozilla");
-
-        System.out.println(browser); // chrome
-    }
-
-    // gradle property test // mozilla
-    //gradle property_test -Dbrowser=opera // opera
-
-    @Test
-    @Tag("hello")
-    void systemPropertiesTest5() {
-        String name = System.getProperty("name", "alice");
-        String message = format("Hello, %s!", name);
-
-        System.out.println(message);
-
-        // gradle hello_test // Hello, alice!
-
-    }
-*/
-
 
 }
